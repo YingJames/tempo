d8937e174761cb76c99398c7fda050165bd0bca4
James_Ying <jyab0617@gmail.com>
2022-09-14 02:02:22 +0000

    add functional nav sidebar

---
+++ script.js
@@ -0,0 +1,8 @@
+const navMenu = document.querySelector('.nav--sidebar');
+function openNav() {
+    navMenu.style.display = "block";
+}
+
+function closeNav() {
+    navMenu.style.display = "none";
+}
